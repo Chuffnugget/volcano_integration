16735e422b313c6080833314f906c99c7d2e58eb
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-15 02:42:26 +0000

    Update button.py

--- custom_components/volcano_integration/button.py
+++ custom_components/volcano_integration/button.py
@@ -1,13 +1,14 @@
 from homeassistant.components.button import ButtonEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
 class StartRandomNumberButton(ButtonEntity):
     """Button to start the random number generator."""
 
     def __init__(self, hass):
+        """Initialize the button."""
         self._hass = hass
 
     @property
@@ -16,16 +17,22 @@ class StartRandomNumberButton(ButtonEntity):
         return "Start Random Number Generator"
 
     async def async_press(self):
-        """Start the random number generator."""
+        """Handle button press to start the generator."""
         sensor = self._hass.data[DOMAIN].get("sensor")
         if sensor:
             sensor._running = True
             self._hass.loop.create_task(sensor._generate_random_number())
+            # Update status sensor
+            status_sensor = self._hass.data[DOMAIN].get("status_sensor")
+            if status_sensor:
+                status_sensor.set_running(True)
+
 
 class StopRandomNumberButton(ButtonEntity):
     """Button to stop the random number generator."""
 
     def __init__(self, hass):
+        """Initialize the button."""
         self._hass = hass
 
     @property
@@ -34,10 +41,14 @@ class StopRandomNumberButton(ButtonEntity):
         return "Stop Random Number Generator"
 
     async def async_press(self):
-        """Stop the random number generator."""
+        """Handle button press to stop the generator."""
         sensor = self._hass.data[DOMAIN].get("sensor")
         if sensor:
             sensor.stop()
+            # Update status sensor
+            status_sensor = self._hass.data[DOMAIN].get("status_sensor")
+            if status_sensor:
+                status_sensor.set_running(False)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
