eb5c018d57f4fe17cee1314f6bcc921ce250d7bb
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-15 02:42:13 +0000

    Update sensor.py

--- custom_components/volcano_integration/sensor.py
+++ custom_components/volcano_integration/sensor.py
@@ -1,8 +1,7 @@
-from homeassistant.components.sensor import SensorEntity  # Ensure SensorEntity is imported
-from homeassistant.helpers.entity import Entity
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
 import random
 import asyncio
 from .const import DOMAIN
@@ -11,6 +10,7 @@ class RandomNumberSensor(SensorEntity):
     """Sensor to generate random numbers."""
 
     def __init__(self, hass: HomeAssistant):
+        """Initialize the random number sensor."""
         self._hass = hass
         self._state = None
         self._running = True
@@ -22,7 +22,7 @@ class RandomNumberSensor(SensorEntity):
 
     @property
     def state(self):
-        """Return the current state."""
+        """Return the current random number."""
         return self._state
 
     async def async_added_to_hass(self):
@@ -36,14 +36,52 @@ class RandomNumberSensor(SensorEntity):
         while self._running:
             self._state = random.randint(1, 100)
             self.async_write_ha_state()
+            # Update the status sensor
+            status_sensor = self._hass.data[DOMAIN].get("status_sensor")
+            if status_sensor:
+                status_sensor.set_running(True)
             await asyncio.sleep(1)
 
     def stop(self):
         """Stop the random number generator."""
         self._running = False
+        # Update the status sensor
+        status_sensor = self._hass.data[DOMAIN].get("status_sensor")
+        if status_sensor:
+            status_sensor.set_running(False)
+
+
+class StatusSensor(SensorEntity):
+    """Sensor to represent the generator's running status."""
+
+    def __init__(self, hass: HomeAssistant):
+        """Initialize the status sensor."""
+        self._hass = hass
+        self._state = "Disconnected"  # Initial state
+
+    @property
+    def name(self):
+        """Return the name of the status sensor."""
+        return "Generator Status"
+
+    @property
+    def state(self):
+        """Return the current status."""
+        return self._state
+
+    def set_running(self, running: bool):
+        """Update the status based on whether the generator is running."""
+        self._state = "Connected" if running else "Disconnected"
+        self.async_write_ha_state()
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
-    """Set up the sensor."""
-    sensor = RandomNumberSensor(hass)
-    async_add_entities([sensor])
+    """Set up the sensors."""
+    # Create the random number sensor
+    random_sensor = RandomNumberSensor(hass)
+
+    # Create the status sensor
+    status_sensor = StatusSensor(hass)
+    hass.data[DOMAIN]["status_sensor"] = status_sensor
+
+    async_add_entities([random_sensor, status_sensor])
