f75a36ff3df8cae10a6100b2e77af28b48340afa
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-17 02:18:48 +0000

    Update sensor.py

--- custom_components/volcano_integration/sensor.py
+++ custom_components/volcano_integration/sensor.py
@@ -54,4 +54,19 @@ class PeriodicBluetoothSensor(SensorEntity):
             await asyncio.sleep(self._interval)
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: 
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+):
+    """Set up sensors for the integration."""
+    sensors = [
+        PeriodicBluetoothSensor(
+            hass,
+            "Volcano Current Temperature",
+            TEMPERATURE_UUID,
+            lambda v: int.from_bytes(v, byteorder="little") / 10,
+            0.5,
+        ),
+    ]
+
+    async_add_entities(sensors)
+    _LOGGER.info("Volcano sensors set up.")
