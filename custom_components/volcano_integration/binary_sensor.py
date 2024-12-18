161e23616fb331c35328875c70b54f8694394dab
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-18 02:29:11 +0000

    Update binary_sensor.py

--- custom_components/volcano_integration/binary_sensor.py
+++ custom_components/volcano_integration/binary_sensor.py
@@ -6,7 +6,7 @@ import logging
 from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback, EntityPlatform
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN, Schema
 from .coordinator import GenericBTCoordinator
@@ -17,9 +17,10 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback) -> None:
     """Set up Binary Sensors for Volcano Integration based on a config entry."""
     coordinator: GenericBTCoordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([BluetoothStatusSensor(coordinator)])
+    async_add_entities([
+        BluetoothStatusSensor(coordinator)
+    ])
 
-    platform = EntityPlatform
     platform = hass.helpers.entity_platform.async_get_current_platform()
     platform.async_register_entity_service("write_gatt", Schema.WRITE_GATT.value, "write_gatt")
     platform.async_register_entity_service("read_gatt", Schema.READ_GATT.value, "read_gatt")
